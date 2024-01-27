9cfad57b94ea57755701258a776203103079a4ed
Nikolai O <nik@o84.ru>
2024-01-27 16:00:42 +0300

    feat: add balanced task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -386,8 +386,23 @@ function isContainNumber(num, digit) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let arrLeft = 0;
+  let arrRight = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    arrLeft = 0;
+    for (let j = 0; j < i; j += 1) {
+      arrLeft += arr[j];
+    }
+    arrRight = 0;
+    for (let k = i + 1; k < arr.length; k += 1) {
+      arrRight += arr[k];
+    }
+    if (arrLeft === arrRight) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
