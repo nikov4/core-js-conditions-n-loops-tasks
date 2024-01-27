e69d31e2b5d06519680af655bf66d0353bc06ab7
Nikolai O <nik@o84.ru>
2024-01-27 04:54:25 +0300

    feat: add triangle task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -129,8 +129,17 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === b && a + b > c && c > 0) {
+    return true;
+  }
+  if (b === c && b + c > a && a > 0) {
+    return true;
+  }
+  if (c === a && c + a > b && b > 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
